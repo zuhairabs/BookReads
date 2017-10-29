2c3207f48c3573daa82da36da988ffd78ab1679b
Sajal Sharma <sajal2692@gmail.com>
2017-10-29 12:50:37 +0530

    feature: search with bugs

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import Book from './Book.js'
+
+import * as BooksAPI from './BooksAPI.js'
+
 class SearchBooks extends Component {
+
+  state = {
+    searchResults: [],
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query})
+    BooksAPI.search(this.state.query,20).then((results) => {
+      this.setState({searchResults: results});
+    })
+  }
 
 
   render() {
+    const { query } = this.state
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -22,12 +40,23 @@ class SearchBooks extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+              />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <div>{query}</div>
+          <ol className="books-grid">
+            {this.state.searchResults.map((b) => (
+              <li key={b.id}>
+                <Book details={b} />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
